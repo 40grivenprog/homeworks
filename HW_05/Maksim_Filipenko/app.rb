84f70aca849a19a27079dd1aa64fc1f1ac109b8a
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2019-05-03 15:25:22 +0300

    Update app.rb

--- HW_05/Maksim_Filipenko/app.rb
+++ HW_05/Maksim_Filipenko/app.rb
@@ -5,11 +5,10 @@ require_relative 'web'
 require_relative 'mentor'
 
 max = Student.new('Maksim Filipenko')
-max.make_hw('HW_05')
-max.submit_homework('HW_05')
-
 alx = Mentor.new('Ivan Ivanov')
 alx.subscribe_to_student(max)
-alx.check_homeworks(max)
-alx.notifications('Maksim Filipenko')
+
+max.make_hw('HW_05')
+max.submit_homework('HW_05')
 alx.read_notifications!
+alx.check_homeworks(max)
